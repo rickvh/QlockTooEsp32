25bbc5a535b4083bc9d8589d3f5456e4e2610825
Rick van Hal <r.hal@avisi.nl>
2021-02-02 20:55:03 +0100

    iro colorpicker include werkt - nu goed als 'component' inzetten

--- qlocktoo-portal/src/app/clock/clock.component.ts
+++ qlocktoo-portal/src/app/clock/clock.component.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
+import { NgZone } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { ColorPickerControl, ColorsTable } from '@iplab/ngx-color-picker';
+// import { ColorPickerControl, ColorsTable } from '@iplab/ngx-color-picker';
+import iro from '@jaames/iro';
+// declare var iro: any;
+
+
 
 @Component({
   selector: 'app-clock',
@@ -20,17 +25,34 @@ export class ClockComponent implements OnInit {
     blue: 100
   }
 
-  constructor(private http: HttpClient) { }
+  colorpicker: any;
+  constructor(private http: HttpClient, private ngZone: NgZone) { }
 
-  public colorPickerControl = new ColorPickerControl()
-    .setValueFrom(ColorsTable.yellowgreen)
-    .showPresets()
-    .hideAlphaChannel();
+  // public colorPickerControl = new ColorPickerControl()
+    // .setValueFrom(ColorsTable.yellowgreen)
+    // .showPresets()
+    // .hideAlphaChannel();
 
   ngOnInit(): void {
     this.http.post('/api/clock', { color: this.color }).subscribe(data => {
       console.log(data);
     })
+
+
+    this.colorpicker = iro.ColorPicker('#picker', {})
+    // this.colorPicker = new iro.ColorPicker('#picker', {});
+    // this.colorPicker.on('color:change', this.onColorChange);
+
+    // this.colorPicker.on('color:change', (color: string, changes: any) =>  this.ngZone.run(() => this.onColorChange(color, changes)));
+  }
+
+  ngOnDestroy(): void {
+    // TODO
+  }
+
+  onColorChange(color: string, changes: any) {
+    // this.selectedColor = color.hexString;
+    console.log(color);
   }
 
   colorChanged(): void {
