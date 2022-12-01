2df55386c61d74e71fff5cbe5748f339ad1ef13f
Rick van Hal <r.hal@avisi.nl>
2022-12-01 19:17:44 +0100

    Remove 'version' as part of the version

--- src/buildinfo.cpp
+++ src/buildinfo.cpp
@@ -1,3 +1,3 @@
 #include "buildinfo.h"
 
-const char* qlocktoo::BuildInfo::version = "Version: " STR(VERSION_INFO) " " __DATE__ " " __TIME__;
+const char* qlocktoo::BuildInfo::version = STR(VERSION_INFO) " " __DATE__ " " __TIME__;
